911dcf5cd387eeb0ec9373b8177008a8fd2d5277
tommy-anderson <alexursu@planable.io>
2023-03-27 17:06:25 +0300

    new

--- index.js
+++ index.js
@@ -5,5 +5,4 @@ const github = require('@actions/github');
 const start = Number(core.getInput('start'))
 const end = Number(core.getInput('end'))
 
-// output a stringified array starting with start and ending with end, inclusive
-core.setOutput("time", JSON.stringify(Array.from({length: end - start + 1}, (v, k) => k + start)));
+core.setOutput("outputArr", Array.from({length: end - start + 1}, (v, k) => k + start));
